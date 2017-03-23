96eedd6b1c9c7a8c4ead247e06d283349cf5f3ec
w1reden <wireden@yandex.ru>
2017-03-23 13:33:48 +0200

    Added wrappers for request and responseWriter objects

--- main.go
+++ main.go
@@ -6,30 +6,14 @@ import (
   "io"
 )
 
-func foo(w http.ResponseWriter, r *http.Request) {
+func foo(w rest.ResponseWriter, r *rest.Request) {
   io.WriteString(w, "foo get")
 }
-
-func bar(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, "foo post")
-}
-
-func a(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, "a put")
-}
-
-func b(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, "b delete")
-}
-
 
 func main() {
   router := rest.Router{}
 
   router.GET("/foo", foo)
-  router.POST("/foo", bar)
-  router.PUT("/foo", a)
-  router.DELETE("/foo", b)
 
   http.ListenAndServe(":8080", router)
 }
--- rest/router.go
+++ rest/router.go
@@ -5,41 +5,48 @@ import (
 )
 
 type Router struct {
-  Routes map[string]map[string]func(http.ResponseWriter, *http.Request)
+  Routes map[string]map[string]func(ResponseWriter, *Request)
+}
+
+type ResponseWriter struct {
+  http.ResponseWriter
+}
+
+type Request struct {
+  *http.Request
 }
 
 /* sets a handler for a certain method */
-func (r *Router) setHandler(method, url string, handler func(http.ResponseWriter, *http.Request)) {
+func (r *Router) setHandler(method, url string, handler func(ResponseWriter, *Request)) {
   if r.Routes == nil {
-    r.Routes = make(map[string]map[string]func(http.ResponseWriter, *http.Request))
-    r.Routes[method] = make(map[string]func(http.ResponseWriter, *http.Request))
+    r.Routes = make(map[string]map[string]func(ResponseWriter, *Request))
+    r.Routes[method] = make(map[string]func(ResponseWriter, *Request))
   }
 
   if r.Routes[method] == nil {
-    r.Routes[method] = make(map[string]func(http.ResponseWriter, *http.Request))
+    r.Routes[method] = make(map[string]func(ResponseWriter, *Request))
   }
 
   r.Routes[method][url] = handler
 }
 
 /* sets another GET handler */
-func (r *Router) GET(url string, handler func(http.ResponseWriter, *http.Request)) {
+func (r *Router) GET(url string, handler func(ResponseWriter, *Request)) {
   r.setHandler("GET", url, handler)
 }
 
 /* sets another POST handler */
-func (r *Router) POST(url string, handler func(http.ResponseWriter, *http.Request)) {
+func (r *Router) POST(url string, handler func(ResponseWriter, *Request)) {
   r.setHandler("POST", url, handler)
 }
 
-
 /* sets another PUT handler */
-func (r Router) PUT(url string, handler func(http.ResponseWriter, *http.Request)) {
+func (r Router) PUT(url string, handler func(ResponseWriter, *Request)) {
   r.setHandler("PUT", url, handler)
 }
 
 /* sets another DELETE handler */
-func (r Router) DELETE(url string, handler func(http.ResponseWriter, *http.Request)) {
+func (r Router) DELETE(url string, handler func(ResponseWriter, *Request)) {
   r.setHandler("DELETE", url, handler)
 }
 
@@ -49,7 +56,9 @@ func (r Router) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 
   for url, handler := range r.Routes[method] {
     if req.URL.Path == url {
-      handler(res, req)
+      request := Request{req}
+      responseWriter := ResponseWriter{res}
+      handler(responseWriter, &request)
     }
   }
 }
